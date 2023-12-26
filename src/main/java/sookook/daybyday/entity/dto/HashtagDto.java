882876a58893f8f50f7e20de0b5fda3706672ac0
suyeon <stgood@kookmin.ac.kr>
2023-12-27 05:31:54 +0900

    fix

--- src/main/java/sookook/daybyday/entity/dto/HashtagDto.java
+++ src/main/java/sookook/daybyday/entity/dto/HashtagDto.java
@@ -1,8 +1,17 @@
 package sookook.daybyday.entity.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import sookook.daybyday.entity.Hashtag;
 
 @Getter @Setter
+@NoArgsConstructor
 public class HashtagDto {
+    private String name;
+
+    public HashtagDto(String name) {
+        this.name = name;
+    }
+
 }
