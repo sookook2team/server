a7dc61e0367ffb0c0cc425ad10ca7e197ce32e41
suyeon <stgood@kookmin.ac.kr>
2023-12-27 06:54:46 +0900

    middle

--- src/main/java/sookook/daybyday/repository/LikeHistoryRepository.java
+++ src/main/java/sookook/daybyday/repository/LikeHistoryRepository.java
@@ -1,7 +1,14 @@
 package sookook.daybyday.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import sookook.daybyday.entity.Hashtag;
 import sookook.daybyday.entity.LikeHistory;
 
+import java.util.List;
+
 public interface LikeHistoryRepository extends JpaRepository<LikeHistory, Long> {
+    @Query("SELECT l FROM LikeHistory l WHERE l.member.id= :member_id")
+    List<LikeHistory> findByMemberId(@Param("member_id") Long member_id);
 }
