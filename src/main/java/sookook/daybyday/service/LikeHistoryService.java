c7179086abe2d4830781ecd594a27b8e3c529062
suyeon <stgood@kookmin.ac.kr>
2023-12-27 06:54:46 +0900

    middle

--- src/main/java/sookook/daybyday/service/LikeHistoryService.java
+++ src/main/java/sookook/daybyday/service/LikeHistoryService.java
@@ -2,7 +2,12 @@ package sookook.daybyday.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sookook.daybyday.entity.LikeHistory;
+import sookook.daybyday.entity.Post;
 import sookook.daybyday.repository.LikeHistoryRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,6 +15,11 @@ public class LikeHistoryService {
 
     private final LikeHistoryRepository likeHistoryRepository;
 
+    // 사용자별 좋아요 게시글 조회
+    @Transactional(readOnly = true)
+    public List<LikeHistory> getLikeByMemberId(Long memberId) {
+        return likeHistoryRepository.findByMemberId(memberId);
+    }
 
 
 }
