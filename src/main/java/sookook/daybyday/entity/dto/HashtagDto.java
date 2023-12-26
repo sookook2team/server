5dbd11bf09bb616df7efb3fd3266916268fa7062
suyeon <stgood@kookmin.ac.kr>
2023-12-27 05:30:14 +0900

    add hashtagservice

--- src/main/java/sookook/daybyday/entity/dto/HashtagDto.java
+++ src/main/java/sookook/daybyday/entity/dto/HashtagDto.java
@@ -2,14 +2,7 @@ package sookook.daybyday.entity.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import sookook.daybyday.entity.Hashtag;
 
 @Getter @Setter
-
 public class HashtagDto {
-    private String name;
-
-    public HashtagDto(Hashtag hashtag) {
-        this.name = hashtag.getName();
-    }
 }
--- src/main/java/sookook/daybyday/service/HashTagService.java
+++ src/main/java/sookook/daybyday/service/HashTagService.java
@@ -5,33 +5,62 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sookook.daybyday.entity.Hashtag;
 import sookook.daybyday.repository.HashtagRepository;
+import sookook.daybyday.repository.PostRepository;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class HashTagService {
 
     private final HashtagRepository hashtagRepository;
+    private final PostRepository postRepository;
     private Hashtag hashtag;
+
+
+    // 해시태그 조회
+    // 해당 post에 저장된 해시태그 리스트 조회
+    public List<Hashtag> getHashtags(Long postId) {
+        return hashtagRepository.findByPostId(postId);
+    }
 
     // 해시태그 생성
     // 이미 있다면 -> 있는 것 가져오기
-    // 없다면 -> 해시태그 입력 후 입력값으로 새로 생성하기 -> 단 해시태그는 계속해서 입력할 수 있다.
-    public Hashtag createHashtag(String content)
-    {
-        if (hashtagRepository.existsByHashtag(content))
-        {
-            return hashtagRepository.findByHashtagName(content);
-        }
-        else
-        {
-            Hashtag hashtag = Hashtag.createHashtag(content);
-            return hashtagRepository.save(hashtag);
-        }
-    }
+    // 없다면 -> 해시태그 입력 후 입력값으로 새로 생성하기
+    // 입력값은 띄어쓰기로 구분할 수 있음.
+    public Hashtag[] createHashtag(Long postId, String content) {
+        // 존재하는 post의 해시태그 리스트
+        List<Hashtag> existingHashtags = getHashtags(postId);
 
-    // 해시태그(저장된 리스트) 조회 -> Post의 <List>hashtags
+        // 띄어쓰기로 구분된 해시태그들을 분리
+        String[] hashtagArray = content.split("\\s+");
+        List<Hashtag> hashtags = new ArrayList<>();
+
+        for (String hashtagContent : hashtagArray) {
+            // 이미 존재하는 해시태그인지 확인
+            boolean exists = existingHashtags.stream()
+                    .anyMatch(hashtag -> hashtag.getName().equals(hashtagContent));
+
+            if (exists) {
+                // 이미 존재하는 해시태그라면 해당 해시태그 가져오기
+                Hashtag existingHashtag = existingHashtags.stream()
+                        .filter(hashtag -> hashtag.getName().equals(hashtagContent))
+                        .findFirst()
+                        .orElse(null);
+                // 기존의 해시태그를 사용하거나 추가적인 로직 수행
+                hashtags.add(existingHashtag); // 이미 존재하는 경우 리스트에 추가
+            } else {
+                // 존재하지 않는 해시태그라면 새로 생성 후 저장
+                Hashtag hashtag = Hashtag.createHashtag(hashtagContent);
+                // 추가적인 로직 수행
+                hashtag.setPost(postRepository.findById(postId).orElse(null));
+                hashtags.add(hashtagRepository.save(hashtag)); // 새로 생성한 경우 리스트에 추가
+            }
+        }
+
+        // 배열로 변환
+        return hashtags.toArray(new Hashtag[0]);
+    }
 
 }
