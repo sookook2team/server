fc9d608a4251808da0281033f98e279c756f2e0f
suyeon <stgood@kookmin.ac.kr>
2023-12-27 02:26:56 +0900

    add login

--- src/main/java/sookook/daybyday/repository/MemberRepository.java
+++ src/main/java/sookook/daybyday/repository/MemberRepository.java
@@ -3,8 +3,11 @@ package sookook.daybyday.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sookook.daybyday.entity.Member;
 
+import java.util.Optional;
+
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByEmail(String email);
+    Optional<Member> findByEmail(String email);
 }
